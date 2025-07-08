57458386f767c8ca9a92e775a89e341838388ce9
Quillen911 <hecksoft0@gmail.com>
2025-07-08 23:14:45 +0300

    Tamamlanandı

--- app/Http/Controllers/categoryController.php
+++ app/Http/Controllers/categoryController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Requests\CategoryValidation\ShowCategoryRequest;
 use App\Http\Requests\CategoryValidation\UpdateCategoryRequest;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use App\Models\Category;
 use App\Models\Product;
 
--- app/Http/Controllers/productController.php
+++ app/Http/Controllers/productController.php
@@ -7,7 +7,6 @@ use App\Http\Requests\ProductValidation\UpdateProductRequest;
 use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\Product;
-use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller{
     public function add(){
