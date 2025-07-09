21fe8a994cc37417d2ecf253a242a8159882ba5f
Quillen911 <hecksoft0@gmail.com>
2025-07-09 13:43:17 +0300

    tamam

--- database/migrations/2025_07_07_120000_add_deleted_at_to_users_table.php
+++ database/migrations/2025_07_07_120000_add_deleted_at_to_users_table.php
@@ -6,20 +6,12 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-<<<<<<< HEAD
-=======
-
->>>>>>> DR-1004
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
             $table->softDeletes();
         });
     }
-<<<<<<< HEAD
-=======
-
->>>>>>> DR-1004
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
